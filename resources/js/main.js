0b2c42333d4acf0588ada5a11511d22d6b59fbd4
Jabidgithub <jabid9731@yahoo.com>
2021-01-23 14:33:20 +0600

    First Commit

---
+++ resources/js/main.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function () {
+
+    // sticky menu
+    $(".js--services-section").waypoint(function(direction){
+        if (direction=="down") {
+            $("nav").addClass("sticky");
+        }
+        else {
+            $("nav").removeClass("sticky");
+        }
+    });
+
+    // mix it up portfolio section
+    var mixer = mixitup('.container'); 
+
+    // smooth scroll-behavoius
+    $("a").on('click', function (event) {
+        if (this.hash !=="") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html , body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                    window.location.hash = hash;
+            })
+       } 
+    });
+
+});
+
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+    
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
